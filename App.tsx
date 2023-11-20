30d35925d98ec1f97a4340ca5219f6d302f7d548
mertaln99 <sen.vs.ben55@hotmail.com>
2023-11-20 16:33:20 +0300

    20.11.2023 16:32

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import MainMenu from './src/Pages/MainMenu/MainMenu';
-import TabMenu from './src/Pages/TabMenu/TabMenu';
+// import TabMenu from './src/Pages/TabMenu/TabMenu';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.background}>
-      <TabMenu />
-      <MainMenu />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Menu" component={MainMenu} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
---
+++ src/Pages/DrawerMenu/DrawerMenu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const DrawerMenu = () => {
+  return (
+    <View>
+      <Text>Nabarrr</Text>
+    </View>
+  );
+};
+
+export default DrawerMenu;
+
+const styles = StyleSheet.create({});
