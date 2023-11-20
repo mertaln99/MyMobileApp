6db08af515d620fb6e74bf01198360e2f28c47ba
mertaln99 <sen.vs.ben55@hotmail.com>
2023-11-20 17:02:31 +0300

    20.11.2023 17:02

--- App.tsx
+++ App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import MainMenu from './src/Pages/MainMenu/MainMenu';
-// import TabMenu from './src/Pages/TabMenu/TabMenu';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator();
+import TabMenu from './src/Pages/TabMenu/TabMenu';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Menu" component={MainMenu} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      <TabMenu/>
+      <MainMenu/>
+    </View>
   );
 };
 
