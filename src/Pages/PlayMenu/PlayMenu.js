4bc52b9fea039b0450002fc5325702f2c0148fd8
mertaln99 <sen.vs.ben55@hotmail.com>
2023-11-21 15:24:51 +0300

    21.11.2023 15:24 Clear

---
+++ src/Pages/PlayMenu/PlayMenu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const PlayMenu = () => {
+    return (
+        <View>
+            <Text>
+                Nabarr
+            </Text>
+        </View>
+    )
+}
+
+export default PlayMenu;
